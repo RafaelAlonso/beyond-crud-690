9f46ae98d5c3c352d879101d2d05bb3c5e9f1597
Rafael Alonso <rafael.palonso@gmail.com>
2021-08-18 11:33:56 -0300

    comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,22 @@
 class ReviewsController < ApplicationController
   def new
+    # precisamos do @restaurant para avisar ao simple form que criaremos
+    # "uma review do restaurante", nao apenas "uma review"
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+    # como o restaurante nao eh um campo no form, mas sim uma info que esta
+    # na url, devemos pega-lo do mesmo jeito que fizemos no #new
     @restaurant = Restaurant.find(params[:restaurant_id])
+
     @review = Review.new(review_params)
+
+    # depois de criarmos a review com as infos que o user preencheu no form,
+    # precisamos conecta-la ao restaurante que pegamos da url
     @review.restaurant = @restaurant
+
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
@@ -24,6 +33,8 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
+    # repare que o :restaurant_id nao esta presente aqui. Nao queremos que
+    # o user nos passe essa informacao (pois pegaremos ela da url)
     params.require(:review).permit(:rating, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,17 @@ Rails.application.routes.draw do
     end
     
     # - querer rotas sobre um modelo relacionado a um restaurante especifico
+    # como precisamos saber sobre QUAL restarante o usuario esta criando uma review
+    # (e como nao teremos um campo no formulario perguntando qual eh, vide exemplo
+    # do comentario num post do Instagram), precisamos guardar o id do restaurante
+    # na url.
     resources :reviews, only: [:new, :create]
   end
 
+  # quando vamos destruir uma review, so o que precisamos eh saber o id dela mesma,
+  # nao sendo necessario o id do restaurante a qual ela pertence. Por esse motivo,
+  # o destroy fica do lado de FORA do resources de restaurants
   resources :reviews, only: [:destroy]
-  
+
   root to: 'restaurants#index'
 end
